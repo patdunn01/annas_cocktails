c420c630065664bcb6cd20ca3252483e71b0e739
Patrick Dunn <patdunn01@hotmail.co.uk>
2023-03-21 09:39:29 +0000

    changes directories

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import HelloSvg from './NavBar_Components/home.js';
+import DifferentSvg from './NavBar_Components/different.js'
 
 const Nav = () => {
   return (
     <div className="mainnav">
     <nav className="navbar">
-      <Link to="/annas_cocktails" className="first-navbar-item">Home</Link>
+      <Link to="/annas_cocktails">
+      <div className="button-container">
+      <HelloSvg />
+      </div>
+      </Link>
       
       <Link to="/populardrinks" className="navbar-item">Popular Drinks</Link>
       
-      <Link to="/randomCocktail" className="navbar-item">Something Different</Link>
+      <Link to="/randomCocktail">
+      <div className="button-container">
+      <DifferentSvg />
+      </div>
+      </Link>
       
       <Link to="/blog" className="navbar-item">Blog</Link>
       
